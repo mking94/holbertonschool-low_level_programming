c680d91bb4a7831883aef901e3703ed408a41088
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-17 13:46:29 +0100

    tsk9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,7 @@
  * File: 101-quote.c
  * Auth: Maher Ben Dada
  */
-
 #include <unistd.h>
-
 /**
  * main - Prints 
  *                
@@ -13,6 +11,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
