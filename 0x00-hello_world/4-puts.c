c900b2e9b87a7c51eec8ec2279085cfffb21e02e
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-17 12:08:46 +0100

    tsk4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h> 
-/*first betty program*/
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main (){ 
 puts("\"Programming is like building a multilingual puzzle"); 
 return 0;
