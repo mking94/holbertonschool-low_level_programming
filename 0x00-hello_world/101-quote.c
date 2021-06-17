26c226ef2c018c2c184bd98922bb54005b39125c
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-17 13:44:12 +0100

    tsk9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,18 @@
 /*
-C program
-*/
+ * File: 101-quote.c
+ * Auth: Maher Ben Dada
+ */
+
 #include <unistd.h>
+
 /**
- * Quote
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- *
+ * Return: Always 1.
  */
 int main(void)
 {
-write (2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-return (1);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
 }
