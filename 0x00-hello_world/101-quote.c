c1275b36625cd47ab8aeff64f73dfc4fe858bcc7
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-17 13:35:46 +0100

    tsk9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,6 @@
  */
 int main(void)
 {
-echo("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
-return (0);
+print ("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+return (1);
 }
