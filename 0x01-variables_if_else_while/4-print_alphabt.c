45c662012a65cb4f8fa12ef17890eccbffe9463f
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-18 09:42:26 +0100

    tsk4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i=0;
+
+for(int i=97;i<123;i++){
+if(i==101 || i==113)
+{
+i++;
+}
+putchar((char)i);
+}
+/* your code goes there */
+return (0);
+}
