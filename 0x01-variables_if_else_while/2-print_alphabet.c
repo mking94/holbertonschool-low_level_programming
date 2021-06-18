6a5f4cde13e421050f003ec2781bb5d2f87e073f
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-18 09:17:06 +0100

    tsk2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+
+	for(i=97;i<123;i++){
+  putchar((char)i)
+  }
+	/* your code goes there */
+	return (0);
+}
