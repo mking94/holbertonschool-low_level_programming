5ad4005f1b9e182c97049c2ebd72ebe39c185d0e
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-18 13:36:47 +0100

    tsk1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,15 +11,15 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if( n  == 0)
+if(n == 0)
 {
 printf("The Sting is %d",n);
 }
-else if( n % 10 < 5)
+else if((n%10) < 5)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0",n,n%10);
 }
-else if( n % 10 < 6)
+else if((n%10) < 6)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0",n,n%10);
 }
