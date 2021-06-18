b364ac53b1d1ddfc1824e31a891a77e08e6e8193
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-18 09:47:13 +0100

    tsk7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i=0;
+
+for(int i=122;i>96;i--){
+putchar((char)i);
+}
+/* your code goes there */
+return (0);
+}
