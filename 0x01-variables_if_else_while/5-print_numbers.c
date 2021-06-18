b312e27099284257434de25747216f9d65a89a2a
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-18 09:44:57 +0100

    tsk5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i=0;
+
+for(int i=0;i<10;i++){
+printf("%d",i);
+}
+/* your code goes there */
+return (0);
+}
