0b15441f8b2ac0b5828fc47300248c1d14b16c06
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-18 09:54:46 +0100

    tsk8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char hex[16]="0123456789abcdef";
+
+for(int i=0;i<16;i++){
+putchar(hex[i]);
+}
+/* your code goes there */
+return (0);
+}
