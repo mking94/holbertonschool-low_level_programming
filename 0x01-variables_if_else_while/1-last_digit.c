ee855fce33b130892d72a2e41cb8de5161bbef62
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-18 13:39:17 +0100

    tsk1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,9 +15,9 @@ if (n == 0)
 {
 printf("The Sting is %d", n);
 }
-else if ((n%10) < 5)
+else if ((n%10) > 5)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, n%10);
+printf("Last digit of %d is %d and is great than 5 and not 0", n, n%10);
 }
 else if ((n%10) < 6)
 {
