edc5ffec1454481cb179990064a840d1d9dac062
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-18 12:20:54 +0100

    tsk2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-  char i;
+char i;
 /* for loop */
-for(i='a';i<='z';i++){
+for(i='a';i<='z';i++)
+{
 putchar(i);
 }
 putchar('\n');
