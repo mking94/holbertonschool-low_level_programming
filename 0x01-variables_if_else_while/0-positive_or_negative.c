2c7284b8ec4f2518a02ffbd5c41420921746dc38
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-18 13:50:05 +0100

    tsk1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,3 +18,27 @@ int main(void)
 	}
 	return (0);
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Print last digit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else 
+{
+printf("%d is positive\n", n);
+}
+return (0);
+}
