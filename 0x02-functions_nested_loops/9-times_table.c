86e8d8fdd5362ebcd41f2b8ad6257d9b9c3cc37d
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-22 21:27:39 +0100

    tsk8

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *  Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+ void times_table(void)
+ {
+ int i,j;
+ for (i = 0; i < 10; i++)
+ {
+ for (j = 0; j < 10; j++)
+ {
+ _putchar ((j * i) + '0');
+ if (j != 9)
+ {
+ _putchar (',');
+ }
+ }
+ _putchar ('\n');
+ }
+ }
