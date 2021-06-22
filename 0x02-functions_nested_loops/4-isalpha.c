601b46dea1bff4184c3c1edfaca1d98a2f790954
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-22 18:15:38 +0100

    tsk4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _islower - Checks if a character is letter.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter. 0 otherwise.
+ */
+int _isalpha(int c) 
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
