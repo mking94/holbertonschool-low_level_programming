82bef7ea4417207c9442d56c92d9837c763b647a
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-22 21:34:08 +0100

    tsk8

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,19 +4,20 @@
  *
  * Return: Always 0.
  */
- void times_table(void)
- {
- int i,j;
- for (i = 0; i < 10; i++)
- {
- for (j = 0; j < 10; j++)
- {
- _putchar ((j * i) + '0');
- if (j != 9)
- {
- _putchar (',');
- }
- }
- _putchar ('\n');
- }
- }
+void times_table(void)
+{
+int i,j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+_putchar ((j * i) + '0');
+if (j != 9)
+{
+_putchar (',');
+_putchar (' ');
+}
+} 
+_putchar ('\n');
+}
+}
