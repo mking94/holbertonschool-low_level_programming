e8f52d23df52f090d0b10e7d8793bff447acffe5
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-22 18:27:51 +0100

    tsk5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_sign - Checks if the sign of integer.
+ * @c: The integer to be checked.
+ *
+ * Return: 1 if character is great or equal zero. 0 otherwise.
+ */
+int print_sign(int n)
+{
+if (c >= 0)
+{
+_putchar ('+');
+return (1);
+}
+else if (c < 0)
+{
+_putchar ('-');
+return (0);
+}
+}
