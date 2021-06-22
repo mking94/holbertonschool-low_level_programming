98d89ff539d66141e25de92e1a38150a759972bd
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-22 15:20:03 +0100

    tsk1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+int i;
+for (i = 97; i < 123; i++)
+{
+_putchar ((char)i);
+}
+_putchar ('\n');
 }
