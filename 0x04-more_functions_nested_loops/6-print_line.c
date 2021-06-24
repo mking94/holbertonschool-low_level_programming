452bde3b88d445b7d460b6658d7733620730d1cc
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 13:20:06 +0100

    tsk6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,17 +5,13 @@
 */
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar ('\n'); 
-}
-else
+if (n > 0)
 {
 int i;
 for (i = 0; i < n; i++)
 {
 _putchar ('_');
 }
-_putchar ('\n');
 }
+_putchar ('\n');
 }
