085a6c9bc5fc559097873a1ac633ccae050f3abd
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 10:13:52 +0100

    tsk9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - rints the numbers from 1 to 100, followed by a new line.
+ * Return: alwase 0;
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 101; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf ("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf ("Buzz ");
+}
+else if (i % 5 == 0)
+{
+printf ("Fizz ");
+}
+else
+{
+printf ("%d", i);
+}
+}
+putchar ('\n');
+return (0);
+}
