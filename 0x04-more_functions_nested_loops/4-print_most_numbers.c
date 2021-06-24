cc2aececb1735fe074734644c99f00b490cc64f6
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 12:48:45 +0100

    tsk4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 /**
 * print_most_numbers - print numbers from 0 to 9 except 2 and 4, followed by a new line.
 */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 int i;
 for (i = 0; i < 10; i++)
