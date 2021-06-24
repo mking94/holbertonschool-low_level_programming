faf040c44769eddd375c46cb514c58e6e6a33490
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 13:47:56 +0100

    tsk8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * print_square - prints a square, followed by a new line.
- * @size: the size of square.
- */
+* print_square - prints a square, followed by a new line.
+* @size: the size of square.
+*/
 void print_square(int size)
 {
 if (size <= 0)
@@ -16,9 +16,9 @@ for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
-_putchar ('#');
+_putchar('#');
 }
-_putchar ('\n');
+_putchar('\n');
 }
 }
 }
