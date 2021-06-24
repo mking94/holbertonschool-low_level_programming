0a34b6f2168817252848f2e55652b628a137acf1
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 14:18:05 +0100

    tsk9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,21 @@ for (i = 1; i < 101; i++)
 {
 if (((i % 3) == 0) && ((i % 5) == 0))
 {
-printf ("FizzBuzz");
+printf("FizzBuzz");
 }
-else if ((i % 3) == 0)
+else if (((i % 5) == 0) && ((i % 3) != 0))
 {
-printf ("Buzz ");
+printf("Buzz");
 }
-else if ((i % 5) == 0)
+if (((i % 3) == 0) && ((i % 5) != 0))
 {
-printf ("Fizz ");
+printf("Fizz");
 }
 else
 {
-printf ("%d ", i);
+printf("%d", i);
 }
 }
-putchar ('\n');
+_putchar('\n');
 return (0);
 }
