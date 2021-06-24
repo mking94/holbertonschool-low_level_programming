d7ccc16aaaf93a7931044b434b5ae7f0cd07e2d9
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 13:15:18 +0100

    tsk5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
-
 /**
-* more_numbers - prints 10 times the numbers from 0 to 14, followed by a new line.
+* more_numbers - Prints the numbers 0-14 ten times.
 */
 void more_numbers(void)
 {
-
 int i, j;
 for (j = 0; j <= 9; j++)
 {
