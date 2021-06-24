a40018f64e692903adcdb128bf8a0c8f6b04db5a
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 12:14:57 +0100

    tsk0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,12 @@
 #include "holberton.h"
 /**
-*
 * _isupper - check if the caracter is uppercase.
-*
 * @c: the caracter to be checked.
-*
 * Return: 1 if character is uppercase. 0 otherwise.
-*
 */
 int _isupper(int c)
 {
-if ((c >= 97) && (c < 123))
+if ((c >= 65) && (c < 91))
 {
 return (1);
 }
