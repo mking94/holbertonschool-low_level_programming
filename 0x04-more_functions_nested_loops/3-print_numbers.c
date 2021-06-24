05422f16d04e529038befe27684c252d8c67742e
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 12:32:13 +0100

    tsk3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,14 @@
 /**
 * print_numbers - numbers, from 0 to 9, followed by a new line.
 *
-* Return: alwase 0.
+* Return: none.
 */
 void print_numbers(void)
 {
 int i;
 for (i = 0; i < 10; i++)
 {
-_putchar((char)(48+i));
+_putchar((char)(48 + i));
 }
 _putchar('\n');
-return (0);
 }
