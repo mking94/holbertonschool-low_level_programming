8027ecba97598b0248d54db6f04566b1794b6f83
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 12:28:35 +0100

    tsk2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * mul - multiplies two integers.
- * @a: the first input.
- * @b: the second input.
- * Return: multiplie two inputs.
- */
+* mul - multiplies two integers.
+* @a: the first input.
+* @b: the second input.
+* Return: multiplie two inputs.
+*/
 int mul(int a, int b)
 {
 return (a*b);
