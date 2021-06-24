fc73e8497912354e1544e7b45d2b038c19189be4
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 12:47:21 +0100

    tsk4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * print_numbers - print numbers from 0 to 9 except 2 and 4, followed by a new line.
- */
+* print_most_numbers - print numbers from 0 to 9 except 2 and 4, followed by a new line.
+*/
 void print_most_numbers(void);
 {
 int i;
