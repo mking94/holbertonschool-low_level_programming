a73cc5ae90078c1218034fe45b7f45d95278e620
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 13:00:20 +0100

    tsk5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,13 @@
 void more_numbers(void)
 {
 int i, j;
-for (j = 0; j < 10; j++)
+for (j = 0; j <= 9; j++)
 {
-for (i = 0; i < 15; i++)
+for (i = 0; i <= 14; i++)
 {
 if (i < 10)
 {
-_putchar (i+'0');
+_putchar ((i % 10) + '0');
 }
 else
 {
