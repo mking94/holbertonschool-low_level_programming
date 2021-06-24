7c8caa915cba0d5cdfa4488621c4235a10c8f77c
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 15:03:05 +0100

    holberton

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -7,8 +8,9 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_diagonal(int n);
 void print_line(int n);
-void print_triangle(int size);
+void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 #endif /* HOLBERTON_H */
