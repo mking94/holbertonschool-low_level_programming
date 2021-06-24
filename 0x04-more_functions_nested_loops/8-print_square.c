6cd75feb65e7a47dc55b7d4f0cb041099dd55de8
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 14:02:45 +0100

    tsk8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,7 @@ void print_square(int size)
   
 int i, j;
 
-if (size <= 0)
-{
-_putchar ('\n'); 
-}
-else
+if (size > 0)
 {
 for (i = 0; i < size; i++)
 {
@@ -24,5 +20,10 @@ _putchar('#');
 
 _putchar('\n');
 }
+  
+}
+else
+{
+_putchar('\n');
 }
 }
