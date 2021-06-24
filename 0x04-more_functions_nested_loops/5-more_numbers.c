5c33df745d3bc67fd09aeca52b90ca0697cfb3e0
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 09:26:18 +0100

    tsk5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line.
+ */
+void more_numbers(void)
+{
+int i, j;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 15; i++)
+{
+if (i < 10)
+{
+_putchar (i+'0');
+}
+else
+{
+_putchar ('1');
+_putchar ((i % 10)+'0');
+}
+}
+_putchar ('\n');
+}
+}
