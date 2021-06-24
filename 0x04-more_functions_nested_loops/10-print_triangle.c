fb247a1a6ac29cc3d54b8072744a3b3b18ad1a5f
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-24 15:06:34 +0100

    tsk10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,24 +5,5 @@
 */
 void print_triangle(int size)
 {
-int size =10;
-int i, j;
-if (!(size <= 0))
-{
-for (i = 1; i <= size; i++)
-{
-for (j = 1; j <= size; j++)
-{
-if ((j + i) <= size)
-putchar(' ');
-else
-putchar('#');
-}
-putchar('\n');
-}
-}
-else
-{
-putchar('\n');
-}
+
 }
