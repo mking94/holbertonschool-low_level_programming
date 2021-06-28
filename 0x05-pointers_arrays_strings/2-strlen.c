af3c37b70e07b4b11f5f48c0603d0c9beda512a5
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-28 12:35:59 +0100

    tsk2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* _strlen - string length.
+* @s: the input.
+* Return: length.
+*/
+
+int _strlen(char *s)
+{
+	  int length = 0;
+
+	  while (s[length] != '\0')
+	  length++;
+return (length);
+}
