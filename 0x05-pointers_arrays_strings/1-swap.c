052eb97c3a716c77e31247d50c0d00cc3d52218c
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-28 12:19:48 +0100

    tsk1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
 * swap_int - integers swap.
 * @a: the first input.
 * @b: the second input.
 * Return: void.
 */
+
 void swap_int(int *a, int *b)
 {
 	  int* tmps = a;
+
 	  a = b;
 	  b = tmps;
 }
