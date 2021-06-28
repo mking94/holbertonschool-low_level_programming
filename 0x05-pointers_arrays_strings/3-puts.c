b99678e1e500f10311cdc835daae02c6ff696a2f
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-28 13:00:33 +0100

    tsk3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
 * _puts - print string.
@@ -12,6 +11,8 @@ int _puts(char *str)
 	int i = 0;
 
 	while (str[i] != '\0')
+	{
 	_putchar(str[i]);
+	}
 	_putchar('\n');
 }
