cac4e0dd5b556ee412164f541264a02bb6acca09
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-30 13:05:23 +0100

    contact two string with n character

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+* _strncat: contact two string.
+* @dest: first input.
+* @src: second input.
+* @n: number of character will be copyed.
+* Return: dest.
+*/
+
+char *_strcat(char *dest, char *src, int n)
+{
+int i = 0, max;
+char tmp[strlen(dest)+strlen(scr)];
+while (dest[i] != '\0')
+{
+tmp[i] = dest[i];
+i++;
+}
+i=0;
+if(strlen(src) != 0)
+{
+while (i = n)
+{
+tmp[i+strlen(dest)] = src[i];
+i++;
+}
+}
+tmp[strlen(dest)+strlen(src)] = '\0';
+dest=tmp;
+return dest;
+}
