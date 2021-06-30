9fd394de7dd6b3e648b9efd7cb71ec7e28b02660
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-30 12:35:33 +0100

    contact two string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+* _strcat: contact two string.
+* @dest: first input.
+* @src: second input.
+* Return: dest.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int i=0, max;
+char tmp[strlen(str)+strlen(str1)];
+while(dest[i]!='\0')
+{
+tmp[i]=dest[i];
+i++;
+}
+i=0;
+while(src[i]!='\0')
+{
+tmp[i+strlen(dest)]=src[i];
+i++;
+}
+t[strlen(str)+strlen(str1)]='\0';
+dest=t;
+return dest;
+}
