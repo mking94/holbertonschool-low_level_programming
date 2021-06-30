25bfbf2b01bfa300c5ee8962c2fa24d4363839c4
mking94 <47383456+mking94@users.noreply.github.com>
2021-06-30 14:01:30 +0100

    holberton

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -4,4 +4,5 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *leet(char *);
 #endif /* HOLBERTON_H */
