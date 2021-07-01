f8e13da46bf828e3ba49cd593aaa5f5b671a4bde
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-01 13:29:11 +0100

    hanges lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,12 @@
 #include "holberton.h"
-#include <string.h>
-
 /**
 * string_toupper: changes all lowercase letters of a string to uppercase.
 * @str: the input.
 * Return: str.
 */
-
 char *string_toupper(char *str)
 {
 int i = 0;
-
 while (str[i] != '\0')
 {
 if(str[i] >= 'a' && str[i] <= 'z')
