4ef0e304f7343e844ed13936332fa7b5596efd72
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-01 15:11:36 +0100

    print an interger.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,6 +9,12 @@
 
 void print_number(int n)
 {
+int sing = 1;
+if(n < 0)
+{
+sing = -1;
+n = -1 * n;
+}
 int i = 10, j = 0;
 int lastdigit = n%10;
 while (i < n)
@@ -17,6 +23,8 @@ i = i * 10;
 j++;
 }
 i = i / 10;
+if(sing == -1)
+_putchar('-');
 while (0 < j)
 {
 _putchar((char)(48+(n/i)));
