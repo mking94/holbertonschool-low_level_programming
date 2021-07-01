4ace533a067116861ea2eb0474f3d44e1c195ccc
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-01 12:40:35 +0100

    reverses the content of an array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,13 +10,15 @@
 
 void reverse_array(int *a, int n)
 {
-n--;
-int i = 0;
-while (n / 2 != i)
-{
-a[i] = a[n-i] + a[i];
-a[n-i] = a[i] - a[n-i];
-a[i] = a[i] - a[a-i]; 
-i++;
-}
+	
+	n--;
+	int i = 0;
+	while (n / 2 != i)
+	{
+		a[i] = a[n-i] + a[i];
+		a[n-i] = a[i] - a[n-i];
+		a[i] = a[i] - a[a-i]; 
+		i++;
+	}
+	
 }
