0848dfcc8997cdf80074a3cf3701cef228f62a41
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-01 13:46:12 +0100

    capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 /**
-* cap_string: change a string to upper .
+* cap_string- capitalizes all words of a string.
 * @str: the input.
 * Return: str .
 */
@@ -12,19 +12,19 @@ char *cap_string(char *str)
 int i = 0;
 char tmp[strlen(str)];
 char tmpp[2];
-tmpp[1]='\0';
+tmpp[1] = '\0';
 char separator[11] = ". ,!(){};?,\n,\"";
 while (str[i] != '\0')
 {
 tmp[i] = str[i];
 i++;
 }
-tmp[i]='\0';
+tmp[i] = '\0';
 i = 0;
 while (tmp[i] != '\0')
 {
-tmpp[0]=tmp[i];
-if(strcspn(separator,tmpp) < 11)
+tmpp[0] = tmp[i];
+if (strcspn(separator, tmpp) < 11)
 {
 i++;
 tmp[i] = toupper(tmp[i]);
