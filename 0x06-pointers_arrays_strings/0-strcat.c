959b0dd9eae82f9b1be1358825b8ac27103faed6
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-01 11:51:27 +0100

    contact two string.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-int x = 0, y = 0;
-while (dest[x] != '\0')
-x++;
-while (src[y] != '\0')
-{
-dest[x] = src[y];
-x++;
-y++;
-}
-return (dest);
+	
+	int x = 0, y = 0;
+	
+	while (dest[x] != '\0')
+		x++;
+	
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+
+		return (dest);
 }
