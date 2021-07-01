13542095a436e9fb9223d0436425e08dd43e30fa
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-01 15:16:10 +0100

    display a number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,33 +1,33 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
-* print_number: print an interger.
-* @n: the input.
-* Return: void.
-*/
-
+ *print_number- display a number
+ *@n: integer
+ */
 void print_number(int n)
 {
-if(n < 0)
+if (n / 10 != 0)
 {
-n = -1 * n;
+print_number(n / 10);
+if (n > 0)
+{
+_putchar(n % 10 + '0');
+}
+else
+{
+_putchar(-n % 10 + '0');
+}
+}
+else if (n == 0)
+{
+_putchar('0');
+}
+else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+{
 _putchar('-');
+_putchar(-n % 10 + '0');
 }
-int i = 10, j = 0;
-int lastdigit = n%10;
-while (i < n)
+else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
 {
-i = i * 10;
-j++;
+_putchar((n % 10) + '0');
 }
-i = i / 10;
-while (0 < j)
-{
-_putchar((char)(48+(n/i)));
-n = n % i;
-i = i / 10;
-j--;
-}
-_putchar((char)48+lastdigit);
 }
