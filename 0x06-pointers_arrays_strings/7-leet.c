7b47447b5b2681716101bed0962e25aaa50ab5ae
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-01 14:42:27 +0100

    encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,33 @@
 #include "holberton.h"
-#include <string.h>
-#include <ctype.h>
 /**
-* string_toupper: encodes a string .
+* leet: encodes a string .
 * @str: the input.
 * Return: str after encode.
 */
 
+char upper(char c)
+{
+if(c >= 'a' && c<= 'z')
+c = (char)(int)c-32;
+else 
+c = c;
+retrun (c);
+}
 char *leet(char *str)
 {
 int i = 0;
 char tmp[strlen(str)];
 while (str[i] != '\0')
 {
-if(toupper(str[i]) == 'A')
+if(upper(str[i]) == 'A')
 tmp[i] = '4';
-else if(toupper(str[i]) == 'E')
+else if(upper(str[i]) == 'E')
 tmp[i] = '3';
-else if(toupper(str[i]) == 'O')
+else if(upper(str[i]) == 'O')
 tmp[i] = '0';
-else if(toupper(str[i]) == 'T')
+else if(upper(str[i]) == 'T')
 tmp[i] = '7';
-else if(toupper(str[i]) == 'L')
+else if(upper(str[i]) == 'L')
 tmp[i] = '1';
 else
 tmp[i] = str[i];
