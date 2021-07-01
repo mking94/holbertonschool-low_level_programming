ff8bd80661a8bc8229dfb77ee0595d379903b53d
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-01 09:40:24 +0100

    encodes a string with rot algorithm

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+* rot13: encodes a string .
+* @str: the input.
+* Return: str after encode.
+*/
+
+char *rot13(char *str)
+{
+int i = 0;
+char tmp[strlen(str)];
+while (str[i] != '\0')
+{
+tmp[i] = str[i];
+i++;
+}  
+tmp[i]='\0';
+i = 0;
+while (tmp[i] != '\0')
+{
+if(toupper(tmp[i]) >= 'A' && toupper(tmp[i]) <= 'Z')
+{
+if(toupper(tmp[i]) >= 'N')
+tmp[i] = (char)tmp[i]-13;
+else
+tmp[i] = (char)tmp[i]+13;
+}
+i++;
+}
+str = tmp;
+return str;
+}
