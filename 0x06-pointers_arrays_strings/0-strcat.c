b3788664048762d67eb83128c901d9b0ef9ecb05
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-01 11:40:55 +0100

    contact two string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <string.h>
 
 /**
@@ -10,20 +11,26 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
-char tmp[strlen(dest)+strlen(src)];
-while(dest[i] != '\0')
-{
-tmp[i] = dest[i];
-i++;
-}
-i = 0;
-while(src[i] != '\0')
-{
-tmp[i+strlen(dest)] = src[i];
-i++;
-}
-tmp[strlen(dest)+strlen(src)] = '\0';
-dest = tmp;
-return dest;
+	int i = 0;
+	
+	char tmp[strlen(dest)+strlen(src)];
+	
+	while(dest[i] != '\0')
+	{
+		tmp[i] = dest[i];
+		i++;
+	}
+	i = 0;
+	
+	while(src[i] != '\0')
+	{
+		tmp[i+strlen(dest)] = src[i];
+		i++;
+	}
+	
+	tmp[strlen(dest)+strlen(src)] = '\0';
+	
+	dest = tmp;
+	
+	return (dest);
 }
