120ab73b8481711d421a51d964c6f28860652f4d
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-05 12:37:38 +0100

    Function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,24 +1,21 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
-* _memset- fills memory with a constant byte.
-* @s: first input;
-* @b: seconde input.
-* @n: third input;
-* Return: s.
-*/
-
+  * _memset - Fill memory with a constant byte
+  * @s: memory area to fill
+  * @b: constant byte to fill
+  * @n: bytes of memory area to fill
+  *
+  * Return: the memory area filled
+  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	char tmp[n];
+	unsigned int a;
 
-	for (i = 0; i < n; i++)
+	for (a = 0; a < n; a++)
 	{
-		tmp[i] = b;
+		s[a] = b;
 	}
-	tmp[i + 1] = '\0';
-	s = tmp;
+
 	return (s);
 }
