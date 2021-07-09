8de0699f6305958ad1eba1617457fc8cbc0be9be
mking94 <47383456+mking94@users.noreply.github.com>
2021-07-09 22:14:53 +0100

    file

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_isupper- uppercase
+ *@c: character
+ *Return: return 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
