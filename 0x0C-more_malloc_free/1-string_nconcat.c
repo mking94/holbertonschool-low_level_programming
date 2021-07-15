e86dcd20c7045f52ab05b3b9a120936be0d8c725
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-07-15 17:25:33 +0200

    Function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  * string_nconcat - concat n bytes from s2 onto s1 in a new memory space
  * @s1: string one
@@ -13,21 +12,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
 unsigned int i;
-if (n > strlen(s2))
-unsigned int len = strlen(s1)+strlen(s2);
+unsigned int len ;
+if (n > longstr(s2))
+len = longstr(s1) + longstr(s2);
 else
-unsigned int len = strlen(s1)+n;
+len = strlen(s1)+n;
 char *tmp = malloc(len);
 for (i = 0; i < len; i++)
 {
-if (i < strlen(s1))
+if(i < longstr(s1))
 {
 tmp[i] = s1[i];
 }
 else {
-tmp[i] = s2[i-strlen(s1)];   
+tmp[i] = s2[i-longstr(s1)];   
 }
 }
 tmp[i] = '\0';
 return (tmp);
+}
+/**
+* longstr - Function count string length
+* @str: input
+* Return: str length
+*/
+int longstr(char *str)
+{
+int i = 0;
+while(str[i] != '\0')
+i++;
+return (i);
 }
