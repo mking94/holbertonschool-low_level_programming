b995e45038736d4cbc3426218190ae4bc10387a4
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-07-15 13:43:20 +0100

    Function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,43 @@
+
+
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * string_nconcat - Function that concatenates two strings.
- * @s1: String 1.
- * @s2: String 2.
- * @n: Bytes from s2.
+ * string_nconcat - concat n bytes from s2 onto s1 in a new memory space
+ * @s1: string one
+ * @s2: string two
+ * @n: number of bytes from s2 to concat onto s1
  *
- * Return: Pointer to the array or NULL.
+ * Return: pointer to new string, NULL on fail
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0, m = 0, q = 0;
-	char *a;
+	char *s;
+	unsigned int i, j, len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (*(s1 + i))
-		i++;
-	while (*(s2 + j))
-		j++;
 
-	if (n >= j)
-		n = j;
+	for (len1 = 0; *(s1 + len1); len1++)
+		;
+	for (len2 = 0; *(s2 + len2); len2++)
+		;
+	len2++;
 
-	a = malloc(sizeof(char) * (i + n + 1));
-
-	if (a == NULL)
+	if (n > len2)
+		s = malloc((len1 + len2) * sizeof(*s));
+	else
+		s = malloc((len1 + n + 1) * sizeof(*s));
+	if (s == NULL)
 		return (NULL);
-	for (k = 0; k <= i; k++)
-	{
-		*(a + k) = *(s1 + l);
-		l++;
-	}
-	for (q = 0; q < n; q++)
-	{
-		*(a + q + i) = *(s2 + m);
-		m++;
-	}
-		*(a + q + i) = '\0';
-	return (a);
+
+	for (i = 0; i < len1; i++)
+		*(s + i) = *(s1 + i);
+	for (j = 0; j < len2 && j < n; i++, j++)
+		*(s + i) = *(s2 + j);
+	*(s + i) = '\0';
+
+	return (s);
 }
