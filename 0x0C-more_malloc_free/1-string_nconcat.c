86d27c455f912dce9a341114c22d7137c1db74c5
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-07-15 17:08:33 +0200

    Function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,32 +10,20 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i, j, len1, len2;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	for (len1 = 0; *(s1 + len1); len1++)
-		;
-	for (len2 = 0; *(s2 + len2); len2++)
-		;
-	len2++;
-
-	if (n > len2)
-		s = malloc((len1 + len2) * sizeof(*s));
-	else
-		s = malloc((len1 + n + 1) * sizeof(*s));
-	if (s == NULL)
-		return (NULL);
-
-	for (i = 0; i < len1; i++)
-		*(s + i) = *(s1 + i);
-	for (j = 0; j < len2 && j < n; i++, j++)
-		*(s + i) = *(s2 + j);
-	*(s + i) = '\0';
-
-	return (s);
+char *s;
+unsigned int i;
+unsigned int len = strlen(s1)+n;
+char *tmp = malloc(len);
+for (i = 0; i < len; i++)
+{
+if(i<strlen(s1))
+{
+tmp[i] = s2[i];
+}
+else {
+tmp[i] = s2[i-strlen(s1)];   
+}
+}
+tmp[i] = '\0';
+return (tmp);
 }
