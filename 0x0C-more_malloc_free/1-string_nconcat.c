7a2338730613b893fbe6ec66a6637f0fe6a5e6f2
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-07-15 17:35:04 +0200

    Function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,12 +11,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
-unsigned int i;
-unsigned int len ;
-if (n > longstr(s2))
-len = longstr(s1) + longstr(s2);
+unsigned int i, lens1 = 0, lens2 = 0;
+while(s1[lens1] != '\0')
+lens1++;
+while(s2[lens2] != '\0')
+lens2++;
+if (n > lens2)
+len = lens1 + lens2;
 else
-len = strlen(s1)+n;
+len = lens1 + n;
 char *tmp = malloc(len);
 for (i = 0; i < len; i++)
 {
@@ -30,16 +33,4 @@ tmp[i] = s2[i-longstr(s1)];
 }
 tmp[i] = '\0';
 return (tmp);
-}
-/**
-* longstr - Function count string length
-* @str: input
-* Return: str length
-*/
-int longstr(char *str)
-{
-int i = 0;
-while(str[i] != '\0')
-i++;
-return (i);
 }
