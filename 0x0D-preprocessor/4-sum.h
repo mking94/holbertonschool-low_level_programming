d31bbb9fa90765ed90575f15dec16d720f4d3256
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-07-19 18:15:38 +0100

    Header file define SUM function

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,5 +1,11 @@
-#ifndef _4_SUM_H
-#define _4_SUM_H
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
+ */
 
 #define SUM(x, y) ((x) + (y))
 
