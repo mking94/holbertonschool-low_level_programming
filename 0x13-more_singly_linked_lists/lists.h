85dc2611f4654630b1b1c384c3bb5414551146a8
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-08-02 09:37:06 +0100

    Header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 size_t print_listint(const listint_t *h);
 #endif /* LISTS_H */
