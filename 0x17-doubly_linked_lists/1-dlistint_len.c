996e4f4549cc590baf1e1ec0033d761e041dbcac
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-09-16 05:14:08 -0400

    Function count number of elements in a linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,9 @@
 #include <lists.h>
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 int i = 0;
 if(h == null)
-return (0);
+return (i);
 else
 {
 while (h->next != null)
