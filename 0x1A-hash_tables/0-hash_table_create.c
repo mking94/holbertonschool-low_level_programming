12408b9192155dd73de4f40aacff429b54e9e109
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-10-08 07:18:41 -0400

    Function that create hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,8 @@ import "hash_tables.h"
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t myHashtable;
+if(size == 0)
+return (NULL);
 myHashtable = malloc(sizeof(hast_table_t*));
 if(myHashtable == NULL)
 retrun (NULL);
