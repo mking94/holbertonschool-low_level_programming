5d467c270d5b3ff6dd329425da8c84b1f54018af
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-05-03 14:35:31 +0200

    Function that searches for a value in an array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
 
 /**
- * binary_search - Binary search algorithm
- * @array: Input array
+ * binary_search - Binary search algorithm.
+ * @array: Input array.
  * @size: The array length.
- * @value: The value that be find its index
- * Return: Index of value in array or -1 
+ * @value: The value that be find its index.
+ * Return: Index of value in array or -1.
 */
 
 int binary_search(int *array, size_t size, int value)
