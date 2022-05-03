b3ac41663912082fe769dbaaee3304e512a08583
mking94 <dadimaher94@gmail.com>
2022-05-03 05:31:33 -0700

    Function that searches for a value in an array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,29 +2,37 @@
 
 /**
  * binary_search - Binary search algorithm
+ * @array: Input array
  * @size: The array length.
- * @value: The value that be find its index;
- */
+ * @value: The value that be find its index
+ * Return: Index of value in array or -1 
+*/
 
 int binary_search(int *array, size_t size, int value)
 {
-    if(array == NULL )
-    {
-        return -1;
-    }
-    int mil, start = 0, end = size;
-    int test = 0; 
-    while(test == 0 && start <= end){
-        mil = (start + end) / 2;
-        if(array[mil] == value){
-            return mil;
-        }
-        else if(value > array[mil]){
-            start = mil + 1;
-        }
-        else {
-            end = mil - 1;
-        }
-    }
-    return -1;
+if (array == NULL)
+{
+return (-1);
+}
+
+int mil, start = 0, end = size;
+int test = 0; 
+
+while (test == 0 && start <= end)
+{
+mil = (start + end) / 2;
+if (array[mil] == value)
+{
+return (mil);
+}
+else if (value > array[mil])
+{
+start = mil + 1;
+}
+else
+{
+end = mil - 1;
+}
+}
+return (-1);
 }
